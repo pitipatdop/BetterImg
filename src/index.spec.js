3f7d9f8f1d88d14fbd8f60335fd32b3f9f9114f0
Pitipat Srichairat <pitipat.dop@gmail.com>
2017-02-21 00:00:17 +0700

    chore(releaseing): Add travis

---
+++ src/index.spec.js
@@ -0,0 +1,10 @@
+const expect = require('chai').expect;
+const BetterImg = require('./index');
+
+describe('BetterImg', function() {
+  describe('all', function() {
+    it('should be an array of value|label', function(){
+      expect(true).to.equal(true);
+    });
+  });
+});
