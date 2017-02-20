cdf96a12bacabc4784fb0e31d035095ada8b351d
Pitipat Srichairat <pitipat.dop@gmail.com>
2017-02-20 10:13:38 +0700

    build script

---
+++ webpack.config.production.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var webpack = require('webpack');
+// var HappyPack = require('happypack');
+
+module.exports = {
+  devtool: 'sourcemap',
+  entry: [
+    './src/BetterImg.js',
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    pathinfo: true,
+    filename: 'index.js',
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(`${process.env.NODE_ENV}`),
+      },
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+      },
+      minimize: true,
+    }),
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, loaders: ['babel-loader'], exclude: /node_modules/, include: __dirname },
+    ],
+  },
+};
